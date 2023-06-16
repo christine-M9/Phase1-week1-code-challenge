7b9866423bc32cd1cc431e642766387ce46d14fa
Christine <christinekhekama99@gmail.com>
2023-06-16 15:21:34 +0300

    first challenge

---
+++ index.js
@@ -0,0 +1,23 @@
+// Prompt input student marks
+const marks = marks ("student marks ( 0 to 100):");
+// Convert the input to a number
+const marksNumber = Number(marks);
+// Confirm the grade based on the marks given
+let grade;
+if (marksNumber > 79) {
+  grade = "A";
+}
+else if (marksNumber >= 60 && marksNumber <= 79) {
+    grade = "B";
+  }
+   else if (marksNumber >= 50 && marksNumber <= 59) {
+    grade = "C";
+  }
+   else if (marksNumber >= 40 && marksNumber <= 49) {
+    grade = "D";
+  } 
+  else {
+    grade = "E";
+  }
+//To get the Output of the grade
+console.log("Grade:", grade);
