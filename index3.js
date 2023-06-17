b304969c783fff780e61b65751a61fcc4abbfe08
Christine <christinekhekama99@gmail.com>
2023-06-17 19:25:51 +0300

    PAYE calculation

---
+++ index3.js
@@ -0,0 +1,14 @@
+//Calculating PAYE
+function calculatePAYE(annualSalary) {
+    if (annualSalary <= 288000) {
+      return (10 / 100) * annualSalary / 12;
+    } 
+    else if (annualSalary <= 388000) {
+      return ((25 / 100) * (annualSalary - 288000) + (10 / 100) * 288000) / 12;
+    }
+     else {
+      return ((30 / 100) * (annualSalary - 388000) + (25 / 100) * 100000 + (10 / 100) * 288000) / 12;
+    }
+  }
+
+  
