9b55ccf0bf73f89aa7663ad4ea9cdaf29a009140
Christine <christinekhekama99@gmail.com>
2023-06-18 20:45:40 +0300

    complete assignment

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Phase1-week1-code-challenge
+
+  # Title
+Phase1-week1-code-challenge.
+
+   ## Description
+Challenge 1: Student Grade Generator (Toy Problem)
+Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+
+A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+ 
+
+Challenge 2: Speed Detector (Toy Problem)
+Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+
+For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+
+ 
+
+Challenge 3: Net Salary Calculator (Toy Problem)
+Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
+NB: Use KRA, NHIF, and NSSF values provided in the link below.
+
+https://www.aren.co.ke/payroll/taxrates.htmLinks to an external site.  
+
+https://www.kra.go.ke/en/individual/calculate-tax/calculating-tax/payeLinks to an external site.
+
+
+   ## Project set-up
+Solutions to the week one code challenge.They are named index1,index2 and index3 in a Js file.
+<!-- For each challenge,run node index.js .Remember to include the number responsible for each test. -->
+For example; the first test run node index1.js when using the terminal.
+
+
+
+   ## Author and License
+   [Christine-M9]
+   (https://github.com/christine-M9)
--- index3.js
+++ index3.js
@@ -1,1 +1,168 @@
+//Calculating NHIF deductions
+
+function calculateDeduction(grossPay) {
+
+  let deduction = 0;
+
+  if (grossPay <=5999 )
+   {
+         deduction = 150;
+  }
+   else if (grossPay >= 6000 && grossPay <= 7999) {
+
+          deduction = 300;
+
+  } 
+  else if (grossPay >= 8000 && grossPay <= 11999) {
+
+          deduction = 400;
+
+  }
+   else if (grossPay >= 12000 && grossPay <= 14999) {
+
+          deduction = 500;
+
+  } 
+  else if (grossPay >= 15000 && grossPay <= 19999) {
+
+         deduction = 600;
+
+  } 
+  else if (grossPay >= 20000 && grossPay <= 24999) {
+
+          deduction = 750;
+
+  } 
+  else if (grossPay >= 25000 && grossPay <= 29999) {
+
+          deduction = 850;
+
+  }
+   else if (grossPay >= 30000 && grossPay <= 34999) {
+
+          deduction = 900;
+
+  }
+   else if (grossPay >= 35000 && grossPay <= 39999) {
+
+          deduction = 950;
+
+  }
+   else if (grossPay >= 40000 && grossPay <= 44999) {
+
+          deduction = 1000;
+
+  }
+   else if (grossPay >= 45000 && grossPay <= 49999) {
+
+          deduction = 1100;
+
+  } 
+  else if (grossPay >= 50000 && grossPay <= 59999) {
+
+         deduction = 1200;
+
+  } else if (grossPay >= 60000 && grossPay <= 69999) {
+
+         deduction = 1300;
+
+  } 
+  else if (grossPay >= 70000 && grossPay <= 79999) {
+
+         deduction = 1400;
+
+  } 
+  else if (grossPay >= 80000 && grossPay <= 89999) {
+
+         deduction = 1500;
+
+  } 
+  else if (grossPay >= 90000 && grossPay <= 99999) {
+
+         deduction = 1600;
+
+  } 
+  else if (grossPay >= 100000) {
+
+        deduction = 1700;
+
+  }
+
+  return deduction;
+
+}
+
+//Calculating NSSF deduction
+
+function calculatePensionPay(pay) {
+
+    return pay * 0.06 
+
+}
+
+
+
+//Rates
+
+function calculateRate(monthlyTaxPay) {
+
+     let rate = 0;
+
+
+
+  if (monthlyTaxPay <= 24000) {
+
+        rate = 10;
+
+  } else if (monthlyTaxPay >= 24001 && monthlyTaxPay <= 32333) {
+
+        rate = 25;
+
+  } else if (monthlyTaxPay > 32333) {
+
+        rate = 30;
+
+  }
+        return rate;
+
+}
+
+//Calculating Net salary
+
+  function calculateNetSalary(salary, benefits){
+
+  let monthlyTaxPay = salary
+
+  let deductTax = calculateRate(monthlyTaxPay)
+
+  let grossPay = salary - ((deductTax / 100) * salary)
+
+  let deductNhif = calculateDeduction(grossPay)
+
+  let pay = salary - ((deductTax / 100) * salary) - deductNhif
+
+  let deductNssf = calculatePensionPay(pay)
+
+  let netSalaryCalculation = () =>{let netSalary = (pay - deductNhif) ; return netSalary}
+
+  let netSalary = netSalaryCalculation()
+
+  console.log(netSalary) //output
+
+  return netSalary
+
+  }
+
+// input the gross salary and benefits below
+
+
+let salary = parseInt("Gross salary");
+
+let benefits = parseInt("Benefits");
+
+calculateNetSalary(salary, benefits);
+
+// run node index3.js on terminal
+
+
 
